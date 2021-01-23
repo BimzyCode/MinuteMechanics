2089bf55c20a22054fdda5ca79bef0d61b6a2893
BimzyCode <BimzyCode@gmail.com>
2021-01-23 19:55:28 +1100

    4 - jetpacks

---
+++ 5MinuteMechanics/Assets/Scripts/Jetpack.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jetpack : MonoBehaviour
+{
+    public float maxFuel = 4f;
+    public float thrustForce = 0.5f;
+    public Rigidbody rigid;
+    public Transform groundedTransform;
+    public ParticleSystem effect;
+
+    private float curFuel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curFuel = maxFuel; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Jump") > 0f && curFuel > 0f)
+        {
+            curFuel -= Time.deltaTime;
+            rigid.AddForce(rigid.transform.up * thrustForce, ForceMode.Impulse);
+            effect.Play();
+        }
+        else if(Physics.Raycast(groundedTransform.position, Vector3.down, 0.05f, LayerMask.GetMask("Grounded")) && curFuel < maxFuel)
+        {
+            curFuel += Time.deltaTime;
+            effect.Stop();
+        }
+        else
+        {
+            effect.Stop();
+        }
+    }
+}
