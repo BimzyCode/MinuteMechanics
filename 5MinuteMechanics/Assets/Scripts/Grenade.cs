70683e8e08a7ac08170fbc736d8f7926278704e0
BimzyCode <BimzyCode@gmail.com>
2021-01-23 16:04:31 +1100

    3 - Grenades

---
+++ 5MinuteMechanics/Assets/Scripts/Grenade.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public int damage = 20;
+    public float explodeDelay = 2f;
+    public float explodeRadius = 15f;
+    public float explosionForce = 15f;
+    public GameObject explodeEffect;
+
+    private float collDelay = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("EnableCollider", collDelay);
+        Invoke("Explode", explodeDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void EnableCollider()
+    {
+        GetComponent<SphereCollider>().isTrigger = false;
+    }
+
+    private void Explode()
+    {
+        Collider[] collidersHit = Physics.OverlapSphere(transform.position, explodeRadius);
+
+        foreach (Collider collider in collidersHit)
+        {
+            Rigidbody rigid = collider.GetComponent<Rigidbody>();
+            if(rigid != null) rigid.AddExplosionForce(explosionForce, transform.position, explodeRadius, 1f, ForceMode.Impulse);
+
+            Character character = collider.GetComponent<Character>();
+            if (character != null) character.TakeDamage(damage);
+        }
+
+        Instantiate(explodeEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
---
+++ 5MinuteMechanics/Assets/Scripts/GrenadeSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeSpawner : MonoBehaviour
+{
+    public GameObject grenade;
+    public Transform spawnPoint;
+    public float grenadeForce = 12f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            SpawnGrenade();
+        }
+    }
+
+    private void SpawnGrenade()
+    {
+        GameObject grenadeObj = Instantiate(grenade, spawnPoint.position, Quaternion.identity);
+        grenadeObj.GetComponent<Rigidbody>().AddForce(spawnPoint.forward * grenadeForce, ForceMode.Impulse);
+    }
+}
