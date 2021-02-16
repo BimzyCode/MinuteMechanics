5b55c333e7100eed0e4cc81200f5d04d97394ccb
BimzyCode <BimzyCode@gmail.com>
2021-02-16 20:05:20 +1100

    grapple hook

---
+++ MinuteMechanics/Assets/Scripts/6. Grappling Hook/Hook.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hook : MonoBehaviour
+{
+    [SerializeField] float hookForce = 25f;
+
+    Grapple grapple;
+    Rigidbody rigid;
+    LineRenderer lineRenderer;
+
+    public void Initialize(Grapple grapple, Transform shootTransform)
+    {
+        transform.forward = shootTransform.forward;
+        this.grapple = grapple;
+        rigid = GetComponent<Rigidbody>();
+        lineRenderer = GetComponent<LineRenderer>();
+        rigid.AddForce(transform.forward * hookForce, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3[] positions = new Vector3[]
+            {
+                transform.position,
+                grapple.transform.position
+            };
+
+        lineRenderer.SetPositions(positions);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if((LayerMask.GetMask("Grapple") & 1 << other.gameObject.layer) > 0)
+        {
+            rigid.useGravity = false;
+            rigid.isKinematic = true;
+
+            grapple.StartPull();
+        }
+    }
+}
